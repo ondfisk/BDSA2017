74bf511c77b638257dab0ebe2ee63882acbbcf8c
Rasmus Lystrøm <rasmus@lystroem.dk>
2017-09-28 09:23:21 +0200

    Slides updated

--- BDSA2017.Lecture05/Program.cs
+++ BDSA2017.Lecture05/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace BDSA2017.Lecture05
+﻿namespace BDSA2017.Lecture05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
