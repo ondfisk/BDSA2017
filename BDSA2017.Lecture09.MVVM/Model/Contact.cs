336518218cd537882062a935d0a13034a56dd9b3
Rasmus Lystrøm <rasmus@lystroem.dk>
2017-11-02 12:00:21 +0100

    Lecture 09 complete

---
+++ BDSA2017.Lecture09.MVVM/Model/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace BDSA2017.Lecture09.MVVM.Model
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ BDSA2017.Lecture09.MVVM/ViewModels/ContactPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using BDSA2017.Lecture09.MVVM.Model;
+using System.Windows.Input;
+
+namespace BDSA2017.Lecture09.MVVM.ViewModels
+{
+    public class ContactPageViewModel : BaseViewModel
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id; 
+            set { if (_id != value) { _id = value; OnPropertyChanged(); } }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name; 
+            set { if (_name != value) { _name = value; OnPropertyChanged(); } }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set { if (_email != value) { _email = value; OnPropertyChanged(); } }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set { if (_message != value) { _message = value; OnPropertyChanged(); } }
+        }
+
+        private readonly ContactRepository _repository;
+
+        public ICommand BackCommand { get; set; }
+
+        public ICommand SaveCommand { get; set; }
+
+        public ContactPageViewModel()
+        {
+            _repository = new ContactRepository();
+
+            SaveCommand = new RelayCommand(async o => {
+                var contact = new Contact { Name = Name, Email = Email, Message = Message };
+                await _repository.CreateAsync(contact);
+                BackCommand?.Execute(null);
+            });
+        }
+    }
+}
