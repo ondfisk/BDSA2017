14c0b2f6196c0d1c0a0cc3a982aed34fc708ee7d
Rasmus Lystrøm <rasmus@lystroem.dk>
2017-11-16 11:51:09 +0100

    Lecture 10 complete

--- BDSA2017.Lecture10.App/Models/IoCContainer.cs
+++ BDSA2017.Lecture10.App/Models/IoCContainer.cs
@@ -1,13 +1,8 @@
 ﻿using BDSA2017.Lecture10.App.ViewModels;
 using BDSA2017.Lecture10.Common;
-using BDSA2017.Lecture10.Entities;
-using BDSA2017.Lecture10.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.IO;
 using System.Net.Http;
-using Windows.Storage;
 
 namespace BDSA2017.Lecture10.App.Models
 {
@@ -19,26 +14,12 @@ namespace BDSA2017.Lecture10.App.Models
         {
             IServiceCollection services = new ServiceCollection();
 
-            var connectionString = $"Data Source={Path.Combine(ApplicationData.Current.LocalFolder.Path, "futurama.db")}";
-
-            services.AddDbContext<FuturamaContext>(options =>
-               options.UseSqlite(connectionString));
-
-            services.AddScoped<IFuturamaContext, FuturamaContext>();
-            services.AddScoped<ICharacterRepository, EntityFrameworkCharacterRepository>();
-            //services.AddScoped<ICharacterRepository, RestCharacterRepository>();
-            //services.AddScoped<HttpClient>();
+            services.AddScoped<ICharacterRepository, RestCharacterRepository>();
+            services.AddScoped<HttpClient>();
             services.AddScoped<MainPageViewModel>();
             services.AddScoped<CharacterPageViewModel>();
 
-            var provider = services.BuildServiceProvider();
-
-            using (var context = provider.GetService<FuturamaContext>())
-            {
-                context.Database.EnsureCreated();
-            }
-
-            return provider;
+            return services.BuildServiceProvider();
         }
     }
 }
--- BDSA2017.Lecture10.Web/Startup.cs
+++ BDSA2017.Lecture10.Web/Startup.cs
@@ -12,6 +12,13 @@ using BDSA2017.Lecture10.Entities;
 using Microsoft.EntityFrameworkCore;
 using BDSA2017.Lecture10.Models;
 using BDSA2017.Lecture10.Common;
+using Swashbuckle.AspNetCore.Swagger;
+using BDSA2017.Lecture10.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BDSA2017.Lecture10.Web
 {
@@ -27,11 +34,49 @@ namespace BDSA2017.Lecture10.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FuturamaContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddRouting(o =>
+            {
+                o.LowercaseUrls = true;
+            });
+
+            services.Configure<MvcOptions>(o =>
+            {
+                o.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.DocumentFilter<LowerCaseDocumentFilter>();
+            });
+            
+            services.AddDbContext<FuturamaContext>(o =>
+                o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IFuturamaContext, FuturamaContext>();
             services.AddScoped<ICharacterRepository, EntityFrameworkCharacterRepository>();
+
+            var options = new AzureAdOptions();
+            Configuration.Bind("AzureAd", options);
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddOpenIdConnect(o =>
+            {
+                o.ClientId = options.ClientId;
+                o.Authority = $"{options.Instance}{options.TenantId}";
+                o.UseTokenLifetime = true;
+                o.CallbackPath = options.CallbackPath;
+            })
+            .AddJwtBearer(o =>
+            {
+                o.Audience = options.ClientId;
+                o.Authority = $"{options.Instance}{options.TenantId}";
+            })
+            .AddCookie();
 
             services.AddMvc();
         }
@@ -43,6 +88,17 @@ namespace BDSA2017.Lecture10.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var options = new RewriteOptions().AddRedirectToHttps();
+            app.UseRewriter(options);
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API");
+            });
+
+            app.UseAuthentication();
 
             app.UseStaticFiles();
 
